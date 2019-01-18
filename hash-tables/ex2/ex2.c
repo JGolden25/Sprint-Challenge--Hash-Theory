e3c2e8aba0c675771d8deac0173b18d90e836929
Joseph Chretien-Golden <joechre@gmail.com>
2019-01-18 13:46:20 -0600

    examples complete

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -6,11 +6,27 @@
 
 char **reconstruct_trip(Ticket **tickets, int length)
 {
-  HashTable *ht = create_hash_table(16);
+  HashTable *ht = create_hash_table(16); //malloc to be free later
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
 
+  for (int i = 0; i < length; i++) { // make a new hash table with data
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+}
+
+
+
+  for (int i = 0; i < length; i++) { // make new route buffer to return
+    if (i == 0) {
+      route[0] = hash_table_retrieve(ht, "NONE");
+  }
+  else {
+      route[i] = hash_table_retrieve(ht, route[i-1]);
+    }
+}
+  
+  destroy_hash_table(ht);
   return route;
 }
 
@@ -20,8 +36,6 @@ void print_route(char **route, int length)
     printf("%s\n", route[i]);
   }
 }
-
-
 
 #ifndef TESTING
 int main(void)
